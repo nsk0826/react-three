3a82442d0de6ece2e70b0f2e84033bc9941882c0
nsk0826 <nsk.20160401@gmail.com>
2022-02-28 23:17:32 +0900

    add Drei

--- src/drawCanvas.tsx
+++ src/drawCanvas.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import * as Fiber from '@react-three/fiber';
+import * as Drei from '@react-three/drei';
 
 const DrawCanvas: React.FC = () => {
 
   return (
     <React.Suspense fallback={<span>loading...</span>}>
       <Fiber.Canvas>
+      <Drei.PerspectiveCamera makeDefault />
+      <Drei.OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
       </Fiber.Canvas>
     </React.Suspense>
   );
