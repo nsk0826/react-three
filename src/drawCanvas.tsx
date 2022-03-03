f81ebaf47a999f89e5a1cc390797cc4d91fe68b7
nsk0826 <nsk.20160401@gmail.com>
2022-03-03 22:42:46 +0900

    typo

--- src/drawCanvas.tsx
+++ src/drawCanvas.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import * as Fiber from '@react-three/fiber';
 import * as Drei from '@react-three/drei';
-import Model from "./Model";
+import Model from "./model";
 
 const DrawCanvas: React.FC = () => {
 
   return (
     <React.Suspense fallback={<span>loading...</span>}>
       <Fiber.Canvas>
-      <Drei.PerspectiveCamera makeDefault />
+      {/* <Drei.PerspectiveCamera makeDefault /> */}
       <Drei.OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
       <Drei.Stage>
         <Model />
